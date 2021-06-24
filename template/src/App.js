050a4f8d82cc20fbeefca5be7c5340cef9a671a4
甄士刚 <zhenshigang@xdf.cn>
2021-06-24 10:44:04 +0800

    提交

--- template/src/App.js
+++ template/src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import {
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import { Menu } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
+const { SubMenu } = Menu;
 
-function App() {
+class App extends React.Component {
+  handleClick = e => {
+    console.log('click ', e);
+    this.props.history.push(`/${e.key}`)
+  };
+
+  render() {
+    return (
+
+      <div>
+        <div className="header">
+          <div className="handle-btn">
+            <i className="el-icon-s-unfold"></i>
+          </div>
+          <div className="user-name">
+            <i className="iconfont icon-yonghu"></i
+            ><span>剑客</span>
+            <i className="iconfont icon-tuichu"></i>
+          </div >
+        </div >
+        <div className='left'>
+          <div className="title">
+            <p>
+
+            </p>
+            <p className="title-name">海贼王</p>
+          </div>
+          <Menu
+            onClick={this.handleClick}
+            style={{ width: 256 }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+          >
+            <SubMenu key='mywork' icon={<SettingOutlined />} title="我的工作">
+              <Menu.Item key="people">拉人入伙</Menu.Item>
+              <Menu.Item key="target">制定目标</Menu.Item>
+              <Menu.Item key="do">执行下去</Menu.Item>
+              <Menu.Item key="monitor">保障执行</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </div>
+
+        <div className='right'>
+          <Switch>
+            <Route exact path="/people">
+              <People />
+            </Route>
+            <Route path="/do">
+              <Do />
+            </Route>
+            <Route path="/target">
+              <Target />
+            </Route>
+            <Route path="/monitor">
+              <Monitor />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+function People() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>拉人入伙</h2>
     </div>
   );
 }
 
-export default App;
+function Do() {
+  return (
+    <div>
+      <h2>执行下去</h2>
+    </div>
+  );
+}
+
+function Target() {
+  return (
+    <div>
+      <h2>制定目标</h2>
+    </div>
+  );
+}
+function Monitor() {
+  return (
+    <div>
+      <h2>保障执行</h2>
+    </div>
+  );
+}
+export default withRouter(App)
--- template/src/index.js
+++ template/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import './reset.css';
+import './assets/icon/iconfont.css';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
+
 
 
